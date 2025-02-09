3606864d86deed99ec0cead7d734b195ee96eeaa
Hồ Lê Viết Bảo <hovietbao28102004@gmail.com>
2025-02-09 19:54:57 +0700

    deploy

--- server/index.js
+++ server/index.js
@@ -43,51 +43,7 @@ app.set('json spaces', 2)
 // Use GZIP
 app.use(compress())
 
-app.use(function (req, res, next) {
-    // Force SSL
-    if (config.isProd && req.protocol !== 'https') {
-        return res.redirect('https://' + (req.hostname || 'instant.io') + req.url)
-    }
-
-    // Redirect www to non-www
-    if (config.isProd && req.hostname === 'www.instant.io') {
-        return res.redirect('https://instant.io' + req.url)
-    }
-
-    // Use HTTP Strict Transport Security
-    // Lasts 1 year, incl. subdomains, allow browser preload list
-    if (config.isProd) {
-        res.header(
-            'Strict-Transport-Security',
-            'max-age=31536000; includeSubDomains; preload'
-        )
-    }
-
-    // Add cross-domain header for fonts, required by spec, Firefox, and IE.
-    const extname = path.extname(req.url)
-    if (['.eot', '.ttf', '.otf', '.woff', '.woff2'].indexOf(extname) >= 0) {
-        res.header('Access-Control-Allow-Origin', '*')
-    }
-
-    // Prevents IE and Chrome from MIME-sniffing a response. Reduces exposure to
-    // drive-by download attacks on sites serving user uploaded content.
-    res.header('X-Content-Type-Options', 'nosniff')
-
-    // Prevent rendering of site within a frame.
-    res.header('X-Frame-Options', 'DENY')
-
-    // Enable the XSS filter built into most recent web browsers. It's usually
-    // enabled by default anyway, so role of this headers is to re-enable for this
-    // particular website if it was disabled by the user.
-    res.header('X-XSS-Protection', '1; mode=block')
-
-    // Force IE to use latest rendering engine or Chrome Frame
-    res.header('X-UA-Compatible', 'IE=Edge,chrome=1')
-
-    next()
-})
-
-app.use(express.static(path.join(__dirname, '../static')))
+app.use(express.static(path.join(__dirname, '../public')))
 
 app.get('/', dhtMiddleware, function (req, res) {
     const metadataList = req.dht.getAllTorrent();
